114f60f33fcee6f38686d150e0d559af2dbb472e
Jhonatas Flor <jhonatas.flor@dcx.ufpb.br>
2024-05-04 15:40:51 -0300

    first commit

---
+++ create_dimensions.py
@@ -0,0 +1,36 @@
+import psycopg2
+import numpy as np
+
+def get_connection():
+    from dotenv import load_dotenv
+    import os
+    load_dotenv()
+    return psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST")
+    )
+
+def create_dimesions():
+    
+    queries_array = np.array([
+        "CREATE TABLE IF NOT EXISTS dim_fuel (id_fuel INTEGER PRIMARY KEY, fuel_name varchar)",
+        "CREATE TABLE IF NOT EXISTS dim_region (id_region INTEGER PRIMARY KEY, region_name varchar) ",
+        "CREATE TABLE IF NOT EXISTS dim_flag (id_flag INTEGER PRIMARY KEY, flag_type varchar)",
+        "CREATE TABLE IF NOT EXISTS dim_uf (id_uf INTEGER PRIMARY KEY, uf_name varchar,  region_id INTEGER, FOREIGN KEY(region_id) REFERENCES dim_region(id_region))",
+        "CREATE TABLE IF NOT EXISTS dim_city (id_city INTEGER PRIMARY KEY, city_name varchar, uf_id INTEGER, FOREIGN KEY(uf_id) REFERENCES dim_uf(id_uf))",
+        "CREATE TABLE IF NOT EXISTS dim_gas_station (id_gas_station INTEGER PRIMARY KEY, gas_station_name varchar, city_id INTEGER, flag_id INTEGER, FOREIGN KEY(city_id) REFERENCES dim_city(id_city), FOREIGN KEY(flag_id) REFERENCES dim_flag(id_flag))",
+        "CREATE TABLE IF NOT EXISTS measurment (id INTEGER PRIMARY KEY, price varchar, measurement_date DATE, gas_station_id INTEGER, fuel_type_id INTEGER,FOREIGN KEY(gas_station_id) REFERENCES dim_gas_station(id_gas_station),FOREIGN KEY(fuel_type_id) REFERENCES dim_fuel(id_fuel) )"
+    ])
+    
+    [cursor.execute(str(query)) for query in np.nditer(queries_array)]
+
+
+if __name__== "__main__":
+    conn = get_connection()
+    cursor = conn.cursor()
+    create_dimesions()
+    conn.commit()
+    cursor.close()
+    conn.close()
---
+++ download_script.py
@@ -0,0 +1,67 @@
+import os
+import io
+import zipfile
+import datetime
+from tqdm import tqdm
+
+def get_url(year):
+    if year >=2022:
+        return (f"https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/dsas/ca/precos-semestrais-ca.zip" if year == 2022 else f"https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/dsas/ca/ca-{year}-01.zip",
+                f"https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/dsas/ca/ca-{year}-02.zip" if year != current_year else None)
+    
+    
+    return (f"https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/dsas/ca/ca-{year}-01.csv",
+            f"https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/shpc/dsas/ca/ca-{year}-02.csv" if year != current_year else None)
+
+def create_folder(year):
+    if not os.path.exists("csv_data"):
+        os.mkdir("csv_data")
+    
+    if not os.path.exists(f"csv_data/{year}"):
+        os.mkdir(f"csv_data/{year}")
+
+def unzip_csv(year,data):
+    zip_content = io.BytesIO(data)
+    with zipfile.ZipFile(zip_content, 'r') as zip_ref:
+        zip_ref.extractall(f"csv_data/{year}/")
+        filelist= os.listdir(f"csv_data/{year}/")
+        if len(filelist) == 2:
+            for file in filelist:
+                splited_name = file.split("-") if year == 2022 else file.split("_")
+                semester = splited_name[splited_name.__len__()-1].split(".")[0] if year == 2022 else splited_name[splited_name.__len__()-1].split(".")[1]
+                os.rename(f"csv_data/{year}/{file}", f"csv_data/{year}/preco_combustivel_{year}_{semester}.csv")
+
+def download_csv(year):
+    urls_tuple = get_url(year)
+    
+    create_folder(year)
+    semester = 1
+    import requests
+    for url in urls_tuple:
+        tqdm.write(f"Downloading from: {url}")
+        response = requests.get(url)
+        if not response.ok:
+            tqdm.write(f"Something got wrong downloading: {url}")
+            return False
+        
+        filename = f"csv_data/{year}/preco_combustivel_{year}_{semester}.csv"
+        if year >= 2022:
+            unzip_csv(year,response.content)
+            tqdm.write(f"Donwloaded: {year}/{semester}")
+            semester+=1
+            continue
+        
+        with open(filename, "wb") as file:
+            file.write(response.content)
+
+        tqdm.write(f"Donwloaded: {year}/{semester}")
+        semester+=1
+    
+    return True
+       
+
+date = datetime.date.today()
+current_month = date.month
+current_year = date.year
+years_list = list(range(2004,current_year+1)) if current_month>6 else list(range(2004,current_year))
+url_list = list(map(download_csv,years_list))
