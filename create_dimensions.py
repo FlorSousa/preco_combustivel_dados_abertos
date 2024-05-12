fd52683b6f8f0114a7eaca0e4331e6fc9c2dcfb9
Jhonatas Flor <jhonatas.flor@dcx.ufpb.br>
2024-05-12 04:50:51 -0300

    ETL complete

--- create_dimensions.py
+++ create_dimensions.py
@@ -16,7 +16,7 @@ def get_connection():
 
 def sanitizer_str(string):
     import re
-    return re.sub(re.compile(r"'"),"",string)
+    return re.sub(re.compile(r"'"),"''",string)
 
 def load_dim_flag(dataframe):
     df_flags = dataframe['Bandeira'].unique()
@@ -39,18 +39,50 @@ def load_dim_city(dataframe):
     queries = list(map(lambda city: f"INSERT INTO dim_city(city_name) VALUES ('{sanitizer_str(city)}');", cities_to_insert))
     [cursor.execute(q) for q in queries]
     
-def load_gas_station(dataframe):
-    df_gas_list = dataframe['Revenda'].unique().astype(object)
-    df_gas_list = df_gas_list[~pd.isnull(df_gas_list)]
-    gas_str = ",".join([f"'{sanitizer_str(gas_station)}'" for gas_station in df_gas_list])
-    cursor.execute(f"SELECT * FROM public.dim_city WHERE city_name IN ({gas_str})")
-    gas_db = [gas_station_name[1] for gas_station_name in cursor.fetchall()]
-    station_insert = filter(lambda f: True if sanitizer_str(f) not in gas_db else False, df_gas_list)
-    queries = list(map(lambda station: f"INSERT INTO dim_gas_station(gas_station_name) VALUES ('{sanitizer_str(station)}');", station_insert))
-    [cursor.execute(q) for q in queries]
-
+def replace_by_id(dataframe,dim,field_name) -> None:
+    cursor.execute(f"SELECT * FROM public.{dim}")
+    select_return = cursor.fetchall() 
+    mapeamento = {valor: chave for chave, valor in select_return}
+    dataframe[field_name] = dataframe[field_name].replace(mapeamento)
+    
+    
+def t(dataframe):
+    dim_to_replace = {
+        "dim_city":"Municipio",
+        "dim_fuel":"Produto",
+        #"dim_gas_station":"Revenda",
+        "dim_region":"Regiao - Sigla",
+        "dim_flag":"Bandeira",
+        "dim_uf":"Estado - Sigla"
+    }
+    for dimension in dim_to_replace:
+        replace_by_id(dataframe,dimension,dim_to_replace[dimension])
+    
+    columns_to_rename = {
+        "Regiao - Sigla":"region_id",
+        "Estado - Sigla":"uf_id",
+        "Municipio":"city_id",
+        "Revenda":"gas_station_name",
+        "CNPJ da Revenda":"cnpj_gas_station",
+        "Produto":"fuel_type_id",
+        "Valor de Venda":"price",
+        "Data da Coleta":"measurement_date"
+    }
+    dataframe = dataframe.rename(columns=columns_to_rename)
+    print("Inserting data to DB")
+    query_insert = 'INSERT INTO measurment(price, measurement_date, gas_station_name, cnpj_gas_station, fuel_type_id, uf_id, city_id, region_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+    data_to_insert = dataframe[['price', 'measurement_date', 'gas_station_name', 'cnpj_gas_station', 'fuel_type_id', 'uf_id', 'city_id', 'region_id']].values.tolist()
+    cursor.executemany(query_insert, data_to_insert)
+    try:
+        conn.commit()
+        print("Data inserted")
+    except Exception as error:
+        print(f"Something got wrong: {error}")
+        exit()
+    
 def load_data():
-    columns_to_remove = ["Numero Rua","Nome da Rua","Complemento","Bairro","Cep","Valor de Compra","Unidade de Medida","CNPJ da Revenda"]
+    columns_to_remove = ["Numero Rua","Nome da Rua","Complemento","Bairro","Cep","Valor de Compra","Unidade de Medida"]
+    
     years = [int(year) for year in os.listdir("csv_data")]
     for year in years:
         num_semester = len(os.listdir(f"csv_data/{year}"))
@@ -58,21 +90,28 @@ def load_data():
             chunks = pd.read_csv(f"csv_data/{year}/preco_combustivel_{year}_{semester}.csv", chunksize=10000, delimiter=";")
             chunks = map(lambda chunk: chunk.drop(columns_to_remove, axis=1), chunks)
             cleaned_df = pd.concat(list(chunks))
+            cleaned_df = cleaned_df.dropna()
             load_dim_flag(cleaned_df)
             load_dim_city(cleaned_df)
-            load_gas_station(cleaned_df)
+            t(cleaned_df)
+        print(f"\n{year} inserted in DB")
 
-    
+    print("\nProcess finished")
+        
 
 def create_dimesions():
+    print("Droping Dimensions if already exists")
+    drop_query = 'DROP TABLE IF EXISTS dim_fuel, dim_region, dim_flag, dim_uf, dim_city, measurment'
+    cursor.execute(drop_query)
+    
+    print("Creating Dimensions")
     queries_array =[
         "CREATE TABLE IF NOT EXISTS dim_fuel (id_fuel SERIAL PRIMARY KEY, fuel_name VARCHAR)",
         "CREATE TABLE IF NOT EXISTS dim_region (id_region SERIAL PRIMARY KEY, region_name varchar) ",
         "CREATE TABLE IF NOT EXISTS dim_flag (id_flag SERIAL PRIMARY KEY, flag_type varchar)",
         "CREATE TABLE IF NOT EXISTS dim_uf (id_uf SERIAL PRIMARY KEY, uf_name varchar)",
         "CREATE TABLE IF NOT EXISTS dim_city (id_city SERIAL PRIMARY KEY, city_name varchar)",
-        "CREATE TABLE IF NOT EXISTS dim_gas_station (id_gas_station SERIAL PRIMARY KEY, gas_station_name varchar)",
-        "CREATE TABLE IF NOT EXISTS measurment (id SERIAL PRIMARY KEY, price varchar, measurement_date DATE,gas_station_id INTEGER,fuel_type_id INTEGER,uf_id INTEGER,city_id INTEGER,region_id INTEGER,FOREIGN KEY(gas_station_id) REFERENCES dim_gas_station(id_gas_station),FOREIGN KEY(fuel_type_id) REFERENCES dim_fuel(id_fuel),FOREIGN KEY(uf_id) REFERENCES dim_uf(id_uf), FOREIGN KEY(city_id) REFERENCES dim_city(id_city), FOREIGN KEY(region_id) REFERENCES dim_region(id_region))"
+        "CREATE TABLE IF NOT EXISTS measurment (id SERIAL PRIMARY KEY, price varchar, measurement_date DATE,gas_station_name varchar, cnpj_gas_station varchar,fuel_type_id INTEGER,uf_id INTEGER,city_id INTEGER,region_id INTEGER,FOREIGN KEY(fuel_type_id) REFERENCES dim_fuel(id_fuel),FOREIGN KEY(uf_id) REFERENCES dim_uf(id_uf), FOREIGN KEY(city_id) REFERENCES dim_city(id_city), FOREIGN KEY(region_id) REFERENCES dim_region(id_region))"
     ]
     regions = ["'SE'","'NE'","'CO'","'N'","'S'"]
     queries_array += list(map(lambda region: f"INSERT INTO dim_region(region_name) VALUES ({region});",regions))
@@ -80,16 +119,20 @@ def create_dimesions():
     uf_list = ["'AC'", "'AL'", "'AP'", "'AM'", "'BA'", "'CE'", "'DF'", "'ES'", "'GO'", "'MA'", "'MT'", "'MS'", "'MG'", "'PA'", "'PB'", "'PR'", "'PE'", "'PI'", "'RJ'", "'RN'", "'RS'", "'RO'", "'RR'", "'SC'", "'SP'", "'SE'", "'TO'"]
     queries_array += list(map(lambda uf: f"INSERT INTO dim_uf(uf_name) VALUES ({uf});",uf_list))
 
-    fuel_list = ["'gasolina'","'etanol'","'diesel'"]
+    fuel_list = ["'GASOLINA'","'ETANOL'","'DIESEL'", "'GNV'"]
     queries_array += list(map(lambda fuel:f"INSERT INTO dim_fuel(fuel_name) VALUES ({fuel});",fuel_list))
     list(map(lambda query: cursor.execute(query), queries_array)) 
-  
-    
+    try:
+        conn.commit()
+        print("Dimensions created")
+    except Exception as error:
+        print(f"Something got wrong: {error}")
+        exit()
+        
 if __name__== "__main__":
     conn = get_connection()
     cursor = conn.cursor()
     create_dimesions()
     load_data()
-    conn.commit()
     cursor.close()
     conn.close()
